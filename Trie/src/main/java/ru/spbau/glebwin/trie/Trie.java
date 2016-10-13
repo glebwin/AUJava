5969c78f09ec8183abfc9a793550e6f9207d839f
glebwin <glebwin@gmail.com>
2016-10-13 15:02:16 +0300

    Added modifiers to methods of nested class Node. Fixed import for @NotNull annotations.

--- Trie/src/main/java/ru/spbau/glebwin/trie/Trie.java
+++ Trie/src/main/java/ru/spbau/glebwin/trie/Trie.java
@@ -1,6 +1,6 @@
 package ru.spbau.glebwin.trie;
 
-import com.sun.istack.internal.NotNull;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.util.HashMap;
@@ -9,6 +9,7 @@ import java.util.HashMap;
  * Serializable trie.
  */
 public class Trie implements StreamSerializable {
+    @NotNull
     private Node root;
 
     Trie() {
@@ -17,6 +18,7 @@ public class Trie implements StreamSerializable {
 
     /**
      * Adds element to the trie.
+     *
      * @return True if element wasn't in the trie, false otherwise.
      */
     public boolean add(String element) {
@@ -29,10 +31,10 @@ public class Trie implements StreamSerializable {
 
         Node node = root;
         for (char symbol : element.toCharArray()) {
-            node.increaseSubtreeSize();
+            node.increaseSubtreeTerminalsNumber();
             node = node.goChild(symbol);
         }
-        node.increaseSubtreeSize();
+        node.increaseSubtreeTerminalsNumber();
         node.setTerminal(true);
 
         return true;
@@ -57,6 +59,7 @@ public class Trie implements StreamSerializable {
 
     /**
      * Removes element from the trie.
+     *
      * @return True if element existed in the trie before the operation, false otherwise.
      */
     public boolean remove(String element) {
@@ -69,10 +72,10 @@ public class Trie implements StreamSerializable {
 
         Node node = root;
         for (char symbol : element.toCharArray()) {
-            node.decreaseSubtreeSize();
+            node.decreaseSubtreeTerminalsNumber();
             node = node.goChild(symbol);
         }
-        node.decreaseSubtreeSize();
+        node.decreaseSubtreeTerminalsNumber();
         node.setTerminal(false);
         cleanup(element);
 
@@ -120,6 +123,7 @@ public class Trie implements StreamSerializable {
 
     /**
      * Writes the trie into stream.
+     *
      * @param out The stream to write into.
      */
     @Override
@@ -131,6 +135,7 @@ public class Trie implements StreamSerializable {
     /**
      * Reads the trie from stream.
      * All old data from the trie is removed.
+     *
      * @param in The stream to read from.
      */
     @Override
@@ -147,19 +152,19 @@ public class Trie implements StreamSerializable {
         private boolean terminal;
         private int subtreeTerminalsNumber;
 
-        Node() {
-            children = new HashMap<Character, Node>();
+        public Node() {
+            children = new HashMap<>();
         }
 
-        boolean hasChild(Character character) {
+        public boolean hasChild(Character character) {
             return children.containsKey(character);
         }
 
-        Node getChild(Character character) {
+        public Node getChild(Character character) {
             return children.get(character);
         }
 
-        void removeChild(Character character) {
+        public void removeChild(Character character) {
             children.remove(character);
         }
 
@@ -168,36 +173,36 @@ public class Trie implements StreamSerializable {
          * Creates a new one if it doesn't exist.
          */
         @NotNull
-        Node goChild(Character character) {
+        public Node goChild(Character character) {
             if (!hasChild(character)) {
                 children.put(character, new Node());
             }
             return getChild(character);
         }
 
-        boolean isTerminal() {
+        public boolean isTerminal() {
             return terminal;
         }
 
-        void setTerminal(boolean terminal) {
+        public void setTerminal(boolean terminal) {
             this.terminal = terminal;
         }
 
         /**
          * Increases the number of terminal nodes in the subtree of current node.
          */
-        void increaseSubtreeSize() {
+        public void increaseSubtreeTerminalsNumber() {
             subtreeTerminalsNumber++;
         }
 
         /**
          * Decreases the number of terminal nodes in the subtree of current node.
          */
-        void decreaseSubtreeSize() {
+        public void decreaseSubtreeTerminalsNumber() {
             subtreeTerminalsNumber--;
         }
 
-        int getSubtreeTerminalsNumber() {
+        public int getSubtreeTerminalsNumber() {
             return subtreeTerminalsNumber;
         }
     }
